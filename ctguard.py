b94d954ae64d62b6c224337167b954be342d2430
Bram <warsocket@gmail.com>
2021-07-13 12:02:27 +0200

    merge

--- ctguard.py
+++ ctguard.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-# CTguard
-# Copyright (C) 2017  Bram Staps
-
-# This file is part of CTguard.
-# CTguard is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# CTguard is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with CTguard. If not, see <http://www.gnu.org/licenses/>.
+#CTguard
+#Copyright (C) 2017 Bram Staps
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
 import os
